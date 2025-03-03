0d7d6a6edcb39b430fddba996225cebea271e8a9
sureshbeekhani <sureshbeekhani26@gmail.com>
2025-03-03 04:15:20 -1200

    Added exception handling module

--- bikehelmetdetection/exception/__init__.py
+++ bikehelmetdetection/exception/__init__.py
@@ -1,30 +1,30 @@
-import sys  # Importing the sys module for system-specific parameters and functions
-
-def error_message_detail(error, error_detail: sys):
-    _, _, exc_tb = error_detail.exc_info()  # Fetching exception information using sys.exc_info()
-
-    file_name = exc_tb.tb_frame.f_code.co_filename  # Getting the filename where the exception occurred
-
-    # Constructing the error message with filename, line number, and error description
-    error_message = "Error occurred python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error)
-    )
-
-    return error_message  # Returning the formatted error message
-
+import os
+import sys
 
 class bhdException(Exception):
-    def __init__(self, error_message, error_detail):
-        """
-        :param error_message: error message in string format
-        :param error_detail: details of the error (expected to be an instance of sys)
-        """
-        super().__init__(error_message)  # Calling the parent constructor with the error message
-
-        # Generating detailed error message using the provided error_message_detail function
-        self.error_message = error_message_detail(
-            error_message, error_detail=error_detail
+    def __init__(self, error_message: Exception, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = bhdException.get_detailed_error_message(
+            error_message=error_message,
+            error_detail=error_detail
         )
 
+    @staticmethod
+    def get_detailed_error_message(error_message: Exception, error_detail: sys) -> str:
+        _, _, exec_tb = error_detail.exc_info()
+        try_block_line_number = exec_tb.tb_lineno
+        file_name = exec_tb.tb_frame.f_code.co_filename
+        error_message = f"""
+        Error occurred in script: [ {file_name} ] at 
+        try block line number: [{try_block_line_number}] 
+        error message: [{error_message}]
+        """
+        return error_message
+
     def __str__(self):
-        return self.error_message  # Returning the detailed error message as a string
+        return self.error_message
+
+    def __repr__(self):
+        return str(bhdException.__name__)
+
+__all__ = ['bhdException']
